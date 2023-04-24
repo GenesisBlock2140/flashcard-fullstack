ba13499fbb70712af3cf291dd96e12ce515e4a20
NathanCb <combnathan02@gmail.com>
2023-04-24 23:57:39 +0200

    Install clsx + refacto Button component for reusability

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -1,29 +1,75 @@
 import { FC } from 'react'
+import { clsx } from 'clsx'
+import Link from 'next/link'
 
 interface IButton {
   text: string
   color: string
+  size?: string
+  to?: string
   icon?: string
+  state?: string
   action?: () => void
 }
 
-const btnColors = {
-  black: 'bg-dark text-white',
-  white: 'bg-white text-dark'
+interface ButtonStyle {
+  base: string
+  state: {
+    disabled: string
+  }
+  colors: { 
+    black: string
+    white: string
+    [key: string]: string
+  }
+  size: {
+    normal: string
+    big: string
+    fit: string
+    [key: string]: string
+  }
 }
 
-const btnDefaultStyle = 'w-[140px] text-xl px-4 py-2 rounded-lg border-[3px] border-dark'
+const styles:ButtonStyle = {
+  base: 'text-xl font-medium px-4 py-2 rounded-lg border-[3px] border-dark transition ease-in-out duration-500',
+  state: {
+    disabled: 'cursor-not-allowed opacity-50'
+  },
+  colors: {
+    black: 'bg-dark text-white hover:bg-white hover:text-dark',
+    white: 'bg-white text-dark hover:bg-dark hover:text-white'
+  },
+  size: {
+    normal: "w-[140px]",
+    big: "w-[200px]",
+    fit: "w-fit"
+  }
+}
 
-export const Button: FC<IButton> = ({text, color, icon, action}) => {
+export const Button: FC<IButton> = ({text, color, size, to, icon, state, action}) => {
 
-  let selectedColor = color === 'black' ? btnColors['black'] : btnColors['white']
-
-  return (
+  const ButtonElement = (
     <button 
-      className={`${btnDefaultStyle} ${selectedColor}`}
+      className={clsx([styles.base, 
+      color && styles.colors[color],
+      state && styles.state.disabled,
+      size ? styles.size[size] : styles.size['normal'],])}
       onClick={action}
+      disabled={state === 'disabled'}
     >
       {text}
     </button>
+  )
+
+  if(!to) {
+    return (
+      ButtonElement
+    )
+  }
+
+  return (
+    <Link href={to}>
+      {ButtonElement}
+    </Link>
   )
 }
